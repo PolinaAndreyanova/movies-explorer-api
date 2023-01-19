a40d072a8602405cfb44644be3000db85ed600dc
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-19 14:41:37 +0300

    feat: создать роуты для user и movie

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { Joi, celebrate, errors } = require('celebrate');
+
+const userRouter = require('./routes/user');
+const movieRouter = require('./routes/movie');
+
+const { checkAuth } = require('./middlewares/auth');
+
+const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/users', checkAuth, userRouter);
+
+app.use('/movies', checkAuth, movieRouter);
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Путь не найден'));
+});
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({ message: statusCode === 500 ? 'Произошла ошибка' : message });
+
+  next();
+});
 
 mongoose.connect('mongodb://127.0.0.1/bitfilmsdb', { useNewUrlParser: true })
   .then(() => {
---
+++ routes/movie.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+const { getMovies, createMovie, deleteMovie } = require('../controllers/movie');
+
+router.get('/', getMovies);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required(),
+    trailer: Joi.string().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required(),
+    movieId: Joi.string().required(),
+  }),
+}), createMovie);
+
+router.delete('/:_id', celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().hex().alphanum().length(24),
+  }).unknown(true),
+}), deleteMovie);
