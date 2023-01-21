50308321aca0507fd5e2d9108d527d941866099b
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-21 12:39:27 +0300

    feat: изменить хранение адреса БД

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const NotFoundError = require('./errors/not-found-error');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV, MONGO_DB } = process.env;
 
 const app = express();
 
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1/bitfilmsdb', { useNewUrlParser: true })
+mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : 'mongodb://127.0.0.1/bitfilmsdb', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB!');
 
