56e74fcd60fba3d0c917f58575a11969792ff15f
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-02-12 22:20:34 +0300

    feat: добавить CORS

--- app.js
+++ app.js
@@ -16,32 +16,34 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// const allowedCors = [
-//
-// ];
+const allowedCors = [
+  'http://localhost:3000',
+  'http://diploma.polina.students.nomoredomains.rocks',
+  'https://diploma.polina.students.nomoredomains.rocks',
+];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
+app.use((req, res, next) => {
+  const { origin } = req.headers;
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
-//   const { method } = req;
+  const { method } = req;
 
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-//   const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
 
-//     return res.end();
-//   }
+    return res.end();
+  }
 
-//   return next();
-// });
+  return next();
+});
 
 app.use(requestLogger);
 
